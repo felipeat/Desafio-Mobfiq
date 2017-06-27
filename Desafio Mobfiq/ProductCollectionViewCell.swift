0aa06c9a3640f1275888386835569b17dd7024e4
Felipe Tavares <felipeat.pro@gmail.com>
2017-06-27 20:25:25 -0300

    Desenho da tela da vitrine, já com o stub do UICollectionViewController e com a UICollecionViewCell customizada.

---
+++ Desafio Mobfiq/ProductCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ProductCollectionViewCell.swift
+//  Desafio Mobfiq
+//
+//  Created by Pro on 27/06/17.
+//  Copyright © 2017 Felipe Tavares. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productPhotoImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var previousPriceLabel: UILabel!
+    @IBOutlet weak var currentPriceLabel: UILabel!
+    @IBOutlet weak var installmentLabel: UILabel!
+    
+}
